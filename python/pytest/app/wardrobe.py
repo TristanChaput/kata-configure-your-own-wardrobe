06f249045428902a898802cc7fb5e190a442c91e
Tristan Chaput <tristan.chaput@arolla.fr>
2022-10-05 14:11:53 +0200

    feat(implement the verification of an existing combination)

--- python/pytest/app/wardrobe.py
+++ python/pytest/app/wardrobe.py
@@ -7,13 +7,28 @@ class Wardrobe:
 
     def __init__(self):
         self.__combinations = []
+        self.configure_wardrobe()
 
     def create_combination(self):
         return [Size.FIFTY, Size.FIFTY, Size.FIFTY, Size.FIFTY, Size.FIFTY]
 
     def add_combination(self, combination: list[Size]):
-        self.__combinations.append(combination)
+        if not self.check_is_exist(combination):
+            self.__combinations.append(combination)
+            return True
+        return False
+
+    def configure_wardrobe(self):
+        self.add_combination(self.create_combination())
 
     def list_of_combinations(self):
-        self.add_combination(self.create_combination())
         return self.__combinations
+
+    def check_is_exist(self, new_combination: list[Size]):
+        for valid_combination in self.__combinations:
+            valid_combination.sort()
+            new_combination.sort()
+            if new_combination == valid_combination:
+                return True
+
+        return False
