be3b160a51d2f5fbc548c7629600aaa157a92392
Tristan Chaput <tristan.chaput@arolla.fr>
2022-10-05 13:19:58 +0200

    test(all combination should be valid)

--- python/pytest/tests/test_wardrobe.py
+++ python/pytest/tests/test_wardrobe.py
@@ -7,3 +7,12 @@ class TestWardrobe:
     def test_is_a_valid_combination(self):
         wardrobe = Wardrobe()
         assert sum(wardrobe.list_of_combinations()[0]) == Wardrobe.WALL_SIZE
+
+    def test_all_valid_combination(self):
+        wardrobe = Wardrobe()
+        valid = True
+        for combination in wardrobe.list_of_combinations():
+            if sum(combination) != Wardrobe.WALL_SIZE:
+                valid = False
+        if not valid:
+            pytest.fail("Combinations are not valid", pytrace=True)
